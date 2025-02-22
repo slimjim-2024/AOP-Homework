6d0e23f63b23436f9170e30c6f98968eac5376ef
Vlad Slimovschi <vlsli24@student.sdu.dk>
2025-02-22 18:13:05 +0100

    added lotsa stuff

--- MainWindow.axaml.cs
+++ MainWindow.axaml.cs
@@ -1,11 +1,111 @@
+using System;
+using System.Collections.Generic;
+using System.IO;
 using Avalonia.Controls;
+using Avalonia.Controls.Shapes;
+using Avalonia.Input;
+using Avalonia.Interactivity;
+using Avalonia.Media;
+using Avalonia.Platform.Storage;
 
 namespace AOP_Homework;
 
 public partial class MainWindow : Window
 {
+    string? openedFile = "AOP_Hork";
+    int height;
+    int width;
+    private char[][] data;
+
     public MainWindow()
     {
         InitializeComponent();
+        Main_Window.Title = "No file opened";
+        Canvas.Width = 800;
+        Canvas.Height = 600;
+        Canvas.Background = Brushes.White;
+    }
+    private void Button_Click(object sender, RoutedEventArgs e)
+    {
+        MessageBox.Text = "Hello, Avalonia!";
+    }
+    private void Canvas_PointerPressed(object sender, PointerPressedEventArgs e)
+    {
+        var point = e.GetPosition(Canvas);
+        int x = (int)point.X / 100;
+        int y = (int)point.Y / 100;
+        if (x < width && y < height)
+        {
+            data[y][x] = data[y][x] == '0' ? '1' : '0';
+            UpdateCanvas(data);
+        }
+    }
+    private async void Load(object sender, RoutedEventArgs e)
+    {
+        var result = await StorageProvider.OpenFilePickerAsync(new FilePickerOpenOptions
+        {
+            Title = "Open a b2img.txt file",
+            FileTypeFilter = new List<FilePickerFileType>
+            {
+                new FilePickerFileType("b2img.txt files")
+                {
+                    Patterns = new[] { "*.b2img.txt" }
+                }
+            },
+            SuggestedStartLocation = await StorageProvider.TryGetFolderFromPathAsync(Environment.CurrentDirectory),
+            SuggestedFileName = openedFile,
+            AllowMultiple = false
+        });
+
+        if (result != null && result.Count > 0)
+        {
+            var LocalPath = result[0].Path.AbsolutePath;
+            Main_Window.Title = result[0].Name;
+            LoadFile(LocalPath);
+        }
+    }
+    private void UpdateCanvas(char[][] drawData)
+    {
+        for (int i = 0; i < height; i++)
+        {
+            for (int j = 0; j < width; j++)
+            {
+                Rectangle rect = new Rectangle();
+                rect.Width = 100;
+                rect.Height = 100;
+                rect.Fill = drawData[i][j] switch
+                {
+                    '0' => Brushes.White,
+                    '1' => Brushes.Black,
+                    _ => Brushes.Red
+                };
+                Canvas.SetLeft(rect, j * 100);
+                Canvas.SetTop(rect, i * 100);
+                Canvas.Children.Add(rect);
+
+            }
+        }
+    }
+    private void LoadFile(string file)
+    {
+        using StreamReader sr = new StreamReader(file);
+        string[] line = sr.ReadLine().Split(' ');
+        height = int.Parse(line[0]);
+        width = int.Parse(line[1]);
+        char[] chardata = sr.ReadToEnd().ToCharArray();
+        data = new char[height][];
+        Canvas.Height = height * 100;
+        Canvas.Width = width * 100;
+        Canvas.Children.Clear();
+        for (int i = 0; i < height; i++)
+        {
+            data[i] = new char[width];
+            for (int j = 0; j < width; j++)
+            {
+                data[i][j] = chardata[i * width + j];
+            }
+        }
+        sr.Close();
+        UpdateCanvas(data);
     }
 }
